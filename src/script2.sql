803c26f24eba410132102b4889814bc3c7039102
Justin Chung <0921justin@gmail.com>
2025-12-18 18:05:15 -0600

    test file_name and repo_name

--- src/script2.sql
+++ src/script2.sql
@@ -11,3 +11,13 @@ VALUES
 -- Person B (D=1->Total 2, R=1->Total 2)
 INSERT INTO module_contributions (engineer_id, module_id, interaction_type)
 VALUES 
+
+-- Person A (D=1, R=1)
+INSERT INTO module_contributions (engineer_id, module_id, interaction_type)
+VALUES 
+    ((SELECT id FROM engineers WHERE email='a@calc.test'), (SELECT id FROM modules WHERE name='RiskModule'), 'DESIGNED'),
+    ((SELECT id FROM engineers WHERE email='a@calc.test'), (SELECT id FROM modules WHERE name='RiskModule'), 'REVIEWED');
+
+-- Person B (D=1->Total 2, R=1->Total 2)
+INSERT INTO module_contributions (engineer_id, module_id, interaction_type)
+VALUES 
